0873a92262092ae69352efd7e50f5db7714fb8c7
Ryunosuke Matsushita <c-rmatsushit@192.168.10.101>
2023-03-07 10:06:18 +0900

    一覧作成

--- src/main/java/com/techacademy/service/EmployeeService.java
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -2,18 +2,21 @@ package com.techacademy.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.techacademy.entity.Employee;
 import com.techacademy.repository.EmployeeRepository;
 
+@Service
 public class EmployeeService {
-	private final EmployeeRepository repository;
+	private final EmployeeRepository employeeRepository;
 
 	public EmployeeService(EmployeeRepository repository) {
-		this.repository = repository;
+		this.employeeRepository = repository;
 	}
 
 	public List<Employee> getEmployeeList() {
 
-		return repository.findAll();
+		return employeeRepository.findAll();
 	}
 }
