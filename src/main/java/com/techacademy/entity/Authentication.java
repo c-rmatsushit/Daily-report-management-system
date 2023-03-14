e68e3dc02e141b02dee2fa137592c84d2863dbde
Ryunosuke Matsushita <c-rmatsushit@192.168.10.101>
2023-03-14 18:54:12 +0900

    質問

--- src/main/java/com/techacademy/entity/Authentication.java
+++ src/main/java/com/techacademy/entity/Authentication.java
@@ -1,6 +1,9 @@
 package com.techacademy.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -13,14 +16,18 @@ import lombok.Data;
 @Table(name = "authentication")
 public class Authentication {
     /** ログインユーザ名 */
+
+	public static enum Role {
+        一般, 管理者
+    }
     @Id
     private String code;
 
     /** パスワード */
     private String password;
 
-    /** 有効日付 */
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     /** ユーザID */
     @OneToOne
