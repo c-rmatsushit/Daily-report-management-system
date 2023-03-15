84188e2bb4b892db87566a73a97662c823da0859
Ryunosuke Matsushita <c-rmatsushit@192.168.10.101>
2023-03-15 12:16:34 +0900

    repotテーブル作成

---
+++ src/main/java/com/techacademy/entity/Report.java
@@ -0,0 +1,50 @@
+package com.techacademy.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.annotations.Type;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "report")
+public class Report {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate reportDate;
+
+	@Column(length = 255, nullable = false)
+	@NotEmpty
+	private String title;
+
+	@Column(nullable = false)
+	@Type(type="text")
+	private String content;
+
+	@Column(nullable = true)
+	public LocalDateTime createdAt;
+
+	@Column(nullable = true)
+	public LocalDateTime updatedAt;
+
+    @OneToOne
+    @JoinColumn(name="employee_id", referencedColumnName="id")
+    private Employee employee;
+
+}
