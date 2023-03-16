ba67783d86cb6c630310ca5cceec52566e57db46
Ryunosuke Matsushita <c-rmatsushit@192.168.10.101>
2023-03-16 09:30:56 +0900

    質問

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -2,9 +2,16 @@ package com.techacademy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.techacademy.entity.Report;
 import com.techacademy.service.ReportService;
 
 @Controller
@@ -23,5 +30,49 @@ public class ReportController {
 
 		return "report/list";
 
+	}
+
+	@GetMapping(value = { "/detail", "/detail/{id}/" })
+	public String getReportdetail(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("report", service.getReport(id));
+		return "report/detail";
+	}
+
+	@PostMapping("/detail")
+	public String postReport(@RequestParam("id") Integer id, @RequestParam("name") String name, Model model) {
+		service.updateReport(id, name);
+		return "redirect:/report/list";
+	}
+
+	@GetMapping("/register")
+	public String getRegister(@ModelAttribute Report report) {
+		return "report/register";
+	}
+
+	@PostMapping("/register")
+	public String postRegister(@Validated Report report, BindingResult res, Model model) {
+		if (res.hasErrors()) {
+
+			return getRegister(report);
+		}
+
+		service.saveReport(report);
+
+		return "redirect:/report/list";
+	}
+
+	@GetMapping("/update/{id}/")
+	public String getReport(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("report", service.getReport(id));
+
+		return "report/update";
+	}
+
+	@PostMapping("/update/{id}/")
+	public String postReport(Report report) {
+
+		service.saveReport(report);
+
+		return "redirect:/report/list";
 	}
 }
--- src/main/java/com/techacademy/service/ReportService.java
+++ src/main/java/com/techacademy/service/ReportService.java
@@ -1,7 +1,14 @@
 package com.techacademy.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.techacademy.entity.Report;
@@ -17,5 +24,42 @@ public class ReportService {
 
 	public List<Report> getReportList() {
 		return reportRepository.findAll();
+	}
+
+	@Transactional
+	public Report saveReport(Report report) {
+		return reportRepository.save(report);
+	}
+
+	public Report getReport(Integer id) {
+		Optional<Report> option = reportRepository.findById(id);
+		Report report = option.orElse(null);
+		report.getEmployee().getName();
+		return report;
+	}
+
+	@Transactional
+	public Report saveRreport(Report report) {
+		LocalDateTime now = LocalDateTime.now();
+		report.setCreatedAt(now);
+		report.setUpdatedAt(now);
+		return reportRepository.save(report);
+	}
+
+	@Transactional
+	public void registerReport(Set<Integer> idck) {
+		for (Integer id : idck) {
+			reportRepository.deleteById(id);
+		}
+	}
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public Report updateReport(Integer id, String name) {
+		Optional<Report> option = reportRepository.findById(id);
+		Report report = option.orElse(null);
+		report.getEmployee().getName();
+		return report;
 	}
 }
